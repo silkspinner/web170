7abe2dceaf4cbd816f10c618836f7245883cc5b3
Ron Nims <rleenims@gmail.com>
2017-05-17 15:08:29 -0700

    Create The Template Files for Your Theme

--- front-page.php
+++ front-page.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 <section id="content">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
-    <h2><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h2>
-    <?php the_content(''); // get page or posting written content ?>
-    <?php endwhile; endif; // end the loop ?>
+    <!-- Main WP loop -->
+    <?php if(have_posts() ) : while(have_posts()) : the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php the_content() ?>
+    <?php endwhile; endif; ?>
+    <!-- END Main WP loop -->
     <small>front-page.php</small>
     
 </section>
